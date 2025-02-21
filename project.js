14668ed12d1c467f604dc2407aeba89a6c8b8e6f
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-02-21 15:31:52 +0100

    start

---
+++ project.js
@@ -0,0 +1,13 @@
+//1. deposit money
+//2. number of lines to ber
+//3. amoutn of money
+//4. spin the machine
+//5. check win
+//6. give user money if win/ take money if lose
+// play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+
+}
