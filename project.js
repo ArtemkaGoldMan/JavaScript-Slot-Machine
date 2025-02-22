5aaa150eef729349747a2e4fc1c5dfa372e54ae3
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-02-22 15:45:37 +0100

    deposit/ line of bet/  bet

--- project.js
+++ project.js
@@ -9,5 +9,53 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount:");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount! Please, TRY AGAIN.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt(
+      "Enter the number of lines(1-3) to bet on(!!! You can choose only one!!!):"
+    );
+    const numberOfLines = parseInt(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines! Please, TRY AGAIN.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance) => {
+    while(true){
+        const bet = prompt("Enter amount of bet. Your max bet is " + balance + "$.");
+        const numberOfBet = parseFloat(bet);
+
+        if(isNaN(numberOfBet) || numberOfBet <=0 || numberOfBet > balance){
+            console.log("Invalid amout of bet! Please, TRY AGAIN.")
+        }
+        else{
+            return numberOfBet;
+        }
+
+    }
 
 }
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet();
+
+console.log(balance);
+console.log(numberOfLines);
+console.log(bet);
